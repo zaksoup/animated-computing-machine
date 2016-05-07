c4782a02275728e2427a97844523e04fcbf81061
Tom Meyer <meyerto@gmail.com>
2016-05-07 07:47:32 -0700

    generalize print, improve input names

--- gen.rb
+++ gen.rb
@@ -36,64 +36,19 @@ private
   end
 end
 
-def make_gate_job(gate_type, name, arg0_name, arg1_name, res_name)
-  {
-    'name' => name,
-    'plan' => [{
-      'aggregate' => [{
-        'get' => 'in1',
-        'trigger' => true,
-        'resource' => arg0_name
-      }, {
-        'get' => 'in2',
-        'trigger' => true,
-        'resource' => arg1_name
-      }],
-    }, {
-      'task' => 'and',
-      'file' => "animated-computing-machine/gates/#{gate_type}.yml",
-    }, {
-      'put' => 'out',
-      'resource' => res_name,
-      'params' => {'file' => 'out/bit'},
-    }]
-  }
-end
-
 class PipelineEngineer
 
   def construct(logic)
     @pipeline = Pipeline.new
-
-    arg1 = logic['print'][0]
-    arg2 = logic['print'][1]
-    arg3 = logic['print'][2]
-
-    eval_print(
-      eval_logic(arg1),
-      eval_logic(arg2),
-      eval_logic(arg3)
-    )
-
+    eval_logic(logic)
     @pipeline
   end
 
-  def eval_gate(gate_type, gate)
-    arg0_name = eval_logic(gate[gate_type][0])
-    arg1_name = eval_logic(gate[gate_type][1])
+private
 
-    name = [gate_type, '_', arg0_name, '_', arg1_name].join
-    res_name = name + '_res'
-
-    @pipeline.add_job make_gate_job(gate_type, name, arg0_name, arg1_name, res_name)
-
-    res_name
-  end
-
-  def eval_print(arg0_name, arg1_name, arg2_name)
-
-    @pipeline.add_job({
-      'name' => 'print',
+  def make_gate_job(gate_type, name, arg0_name, arg1_name, res_name)
+    {
+      'name' => name,
       'plan' => [{
         'aggregate' => [{
           'get' => 'in1',
@@ -103,11 +58,55 @@ class PipelineEngineer
           'get' => 'in2',
           'trigger' => true,
           'resource' => arg1_name
-        }, {
-          'get' => 'in3',
-          'trigger' => true,
-          'resource' => arg2_name
         }],
+      }, {
+        'task' => 'and',
+        'file' => "animated-computing-machine/gates/#{gate_type}.yml",
+      }, {
+        'put' => 'out',
+        'resource' => res_name,
+        'params' => {'file' => 'out/bit'},
+      }]
+    }
+  end
+
+  def eval_gate(gate_type, gate)
+    arg0_name = eval_logic(gate[gate_type][0])
+    arg1_name = eval_logic(gate[gate_type][1])
+
+    name = [gate_type, '(', arg0_name, ',', arg1_name, ')'].join
+    res_name = name + '_res'
+
+    @pipeline.add_job make_gate_job(gate_type, name, arg0_name, arg1_name, res_name)
+
+    res_name
+  end
+
+  def eval_print(node)
+
+    arg_names = node['print'].map{|arg|
+      eval_logic(arg)
+    }
+
+    gets = []
+    inputs = []
+    echo_cmd = 'echo '
+    arg_names.each_with_index.map{|arg_name,i|
+      gets << {
+        'get' => "in#{i}",
+        'trigger' => true,
+        'resource' => arg_name
+      }
+
+      inputs << {'name' => "in#{i}"}
+
+      echo_cmd += "$(cat in#{i}/bit)"
+    }
+
+    @pipeline.add_job({
+      'name' => 'print',
+      'plan' => [{
+        'aggregate' => gets,
       }, {
         'task' => 'print',
         'config' => {
@@ -116,14 +115,10 @@ class PipelineEngineer
             'type' => 'docker-image',
             'source' => {'repository' => 'alpine'}
           },
-          'inputs' => [
-            {'name' => 'in1'},
-            {'name' => 'in2'},
-            {'name' => 'in3'},
-          ],
+          'inputs' => inputs,
           'run' => {
             'path' => 'sh',
-            'args' => ['-exc', 'echo "$(cat in1/bit)$(cat in2/bit)$(cat in3/bit)"']
+            'args' => ['-exc', echo_cmd]
           }
         }
       }]
@@ -135,7 +130,7 @@ class PipelineEngineer
   def eval_set(set_def)
 
     val = set_def['val']
-    name = 'in_' + set_def['in']
+    name = set_def['in']
     res_name = name + '_res'
 
     @pipeline.add_resource({
@@ -176,6 +171,8 @@ class PipelineEngineer
       eval_gate('and', node)
     elsif node['in']
       eval_set(node)
+    elsif node['print']
+      eval_print(node)
     else
       puts 'wtf is this? ' + node.inspect
       exit(1)
